737f68dd220d1979329d8ae13c5a6660ec969c36
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2022-04-23 20:27:02 -0300

    Add files via upload

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,11 +1,14 @@
 const tela = document.querySelector('#tela')
 const btnigual = document.querySelector('.igualdade')
+const btnTrocaSinal = document.querySelector('.trocaSinal')
+const btnponto = document.querySelector('.btnponto')
 const btnnum = document.querySelectorAll('.num')
 const btnop = document.querySelectorAll('.operador')
 
 
 var expressao = []
 var estado = false
+
 
 btnnum.forEach((item, i) => {
     item.addEventListener('click', () => {
@@ -25,6 +28,24 @@ btnop.forEach((item, i) => {
     tela.innerText = ''
   }
   })
+})
+
+btnponto.addEventListener('click', () => {
+  if(tela.innerText.length == 0){return}
+
+  for(i = 0; i < tela.innerText; i++){
+    if(tela.innerText[i] == '.'){return}
+  }
+
+  tela.innerText += '.'
+})
+
+btnTrocaSinal.addEventListener('click', () => {
+  if(tela.innerText == 0){return}
+  if(expressao.length == 1){
+    expressao[0] *= -1
+   }
+  tela.innerText = Number(tela.innerText) * -1
 })
 
 btnigual.addEventListener('click', () => {
